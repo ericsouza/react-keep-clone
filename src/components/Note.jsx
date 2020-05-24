f95c6b7b7d99fdcd89e690835fafd3582901cf03
Eric Souza <ericsouza0801@gmail.com>
2020-05-24 20:05:51 -0300

    started styles, added footer, header and notes

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Do homework</h1>
+      <p>You have to do your math homework</p>
+    </div>
+  );
+}
+
+export default Note;
